fb15389fe7c732f8cfa88e9eb8694050e7e7db5f
王云超 <wangyc@witmob.com>
2015-08-07 11:47:53 +0800

    屏蔽了进度条

--- MediaPlayerDemo/src/com/wyc/mediaplayerdemo/MainActivity.java
+++ MediaPlayerDemo/src/com/wyc/mediaplayerdemo/MainActivity.java
@@ -2,22 +2,22 @@ package com.wyc.mediaplayerdemo;
 
 import io.vov.vitamio.LibsChecker;
 import io.vov.vitamio.MediaPlayer;
-import io.vov.vitamio.Vitamio;
 import io.vov.vitamio.MediaPlayer.OnBufferingUpdateListener;
 import io.vov.vitamio.MediaPlayer.OnInfoListener;
-import io.vov.vitamio.widget.MediaController;
+import io.vov.vitamio.Vitamio;
 import io.vov.vitamio.widget.VideoView;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnInfoListener, OnBufferingUpdateListener {
-
 	/**
 	 * TODO: Set the path variable to a streaming video URL or a local media
 	 * file path.
@@ -27,6 +27,7 @@ public class MainActivity extends Activity implements OnInfoListener, OnBufferin
 	private VideoView mVideoView;
 	private ProgressBar pb;
 	private TextView downloadRateView, loadRateView;
+	private View player_btn;
 
 	@Override
 	public void onCreate(Bundle icicle) {
@@ -37,7 +38,7 @@ public class MainActivity extends Activity implements OnInfoListener, OnBufferin
 		setContentView(R.layout.activity_main);
 		mVideoView = (VideoView) findViewById(R.id.buffer);
 		pb = (ProgressBar) findViewById(R.id.probar);
-
+		player_btn = findViewById(R.id.player_btn);
 		downloadRateView = (TextView) findViewById(R.id.download_rate);
 		loadRateView = (TextView) findViewById(R.id.load_rate);
 		if (path == "") {
@@ -53,26 +54,52 @@ public class MainActivity extends Activity implements OnInfoListener, OnBufferin
 			 */
 			uri = Uri.parse(path);
 			mVideoView.setVideoURI(uri);
-			mVideoView.setMediaController(new MediaController(this));
+			// mVideoView.setMediaController(new MediaController(this));
+
 			mVideoView.requestFocus();
+			mVideoView.setMediaBufferingIndicator(player_btn);
 			mVideoView.setOnInfoListener(this);
 			mVideoView.setOnBufferingUpdateListener(this);
 			mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
 				@Override
 				public void onPrepared(MediaPlayer mediaPlayer) {
 					// optional need Vitamio 4.0
+					Log.e("tag", "---OnPreparedListener---");
 					mediaPlayer.setPlaybackSpeed(1.0f);
 					mediaPlayer.setAudioAmplify(1.0f);
 				}
 			});
 		}
+		player_btn.setOnClickListener(new OnClickListener() {
 
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				mVideoView.start();
+				player_btn.setVisibility(View.GONE);
+			}
+		});
+		mVideoView.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (mVideoView.isPlaying()) {
+					player_btn.setVisibility(View.VISIBLE);
+					mVideoView.pause();
+				} else {
+					mVideoView.start();
+				}
+			}
+		});
 	}
 
 	@Override
 	public boolean onInfo(MediaPlayer mp, int what, int extra) {
 		switch (what) {
 		case MediaPlayer.MEDIA_INFO_BUFFERING_START:
+			Log.e("tag", "---MEDIA_INFO_BUFFERING_START---");
 			if (mVideoView.isPlaying()) {
 				mVideoView.pause();
 				pb.setVisibility(View.VISIBLE);
@@ -84,12 +111,14 @@ public class MainActivity extends Activity implements OnInfoListener, OnBufferin
 			}
 			break;
 		case MediaPlayer.MEDIA_INFO_BUFFERING_END:
+			Log.e("tag", "---MEDIA_INFO_BUFFERING_END---");
 			mVideoView.start();
 			pb.setVisibility(View.GONE);
 			downloadRateView.setVisibility(View.GONE);
 			loadRateView.setVisibility(View.GONE);
 			break;
 		case MediaPlayer.MEDIA_INFO_DOWNLOAD_RATE_CHANGED:
+			Log.e("tag", "---MEDIA_INFO_DOWNLOAD_RATE_CHANGED---");
 			downloadRateView.setText("" + extra + "kb/s" + "  ");
 			break;
 		}
@@ -98,6 +127,7 @@ public class MainActivity extends Activity implements OnInfoListener, OnBufferin
 
 	@Override
 	public void onBufferingUpdate(MediaPlayer mp, int percent) {
+		Log.e("tag", "---onBufferingUpdate---");
 		loadRateView.setText(percent + "%");
 	}
 
@@ -118,4 +148,5 @@ public class MainActivity extends Activity implements OnInfoListener, OnBufferin
 		}
 		return true;
 	}
+
 }
